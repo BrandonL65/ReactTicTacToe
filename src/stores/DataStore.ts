95bf4d2f73fd1bfced996ed80014b1b87ecc0fe8
Brandon Lau <brandonlau@Brandons-MacBook-Pro.local>
2021-01-15 21:06:58 -0500

    add datastore

---
+++ src/stores/DataStore.ts
@@ -0,0 +1,15 @@
+import { observable } from "mobx";
+
+interface Board {
+  top: string[];
+  middle: string[];
+  bottom: string[];
+}
+
+export class DataStore {
+  @observable board: Board = {
+    top: ["N", "N", "N"],
+    middle: ["N", "N", "N"],
+    bottom: ["N", "N", "N"],
+  };
+}
